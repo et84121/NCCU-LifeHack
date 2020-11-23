db96f2e360d92e2e9d9030ef24e51da6d846644a
BunnyBit <f411024@gmail.com>
2020-11-23 16:02:33 +0800

    修正到可以正常compile成功

--- life-hack/src/main.js
+++ life-hack/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 
 import "./plugin-firebase";
 
+import { firestorePlugin } from "vuefire";
+
+Vue.use(firestorePlugin);
+
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
--- life-hack/src/plugin-firebase.js
+++ life-hack/src/plugin-firebase.js
@@ -1,8 +1,6 @@
-import VueFire from "vuefire";
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-Vue.use(VueFire);
 firebase.initializeApp({
   projectId: "YOUR_PROJECT_ID",
   databaseURL: "YOUR_DB_URL"
